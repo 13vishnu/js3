c109309b6aaf37581fe331a4abc9bda483340cbc
13vishnu <114495087+13vishnu@users.noreply.github.com>
2022-10-06 08:10:51 +0100

    flow chart programs

---
+++ flo.js
@@ -0,0 +1,6 @@
+var total=0
+for( var i=0;i<5;i++){
+    var marks=prompt("Enter marks")
+    total=total+parseInt(marks)
+}
+document.write("the total is"+total)
---
+++ flow.js
@@ -0,0 +1,8 @@
+var count = 0;
+while(count<5){
+    var count=prompt('enter the value')
+    document.write( " The value is"+count+"<br>")
+   
+
+    }
+    
